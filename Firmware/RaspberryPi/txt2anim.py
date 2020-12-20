bf7b122fe20226d75854e716266b46d04fdb1b0c
Harish S. Kulkarni <harishsk@microsoft.com>
2020-12-19 23:48:06 -0800

    Added scrolling text animation

--- Firmware/RaspberryPi/txt2anim.py
+++ Firmware/RaspberryPi/txt2anim.py
@@ -1,6 +1,11 @@
 import fire
 from PIL import Image, ImageDraw, ImageFont
 from animation import Animation, IFrame, DFrame, Color
+from sys import platform
+
+ANIMATION_WIDTH = 18
+ANIMATION_HEIGHT = 18
+ANIMATION_SIZE = (ANIMATION_WIDTH, ANIMATION_HEIGHT)
 
 def get_palette(images):
     palette = []
@@ -32,27 +37,80 @@ def images_to_animation(images, delay):
     animation.data = frames
     return animation    
 
-def text_to_animation(text, font_size, color, delay):
-    animation_size = (18, 18)
-    font_path = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf'
-    font = ImageFont.truetype(font_path, font_size)
-    width, height = animation_size
+def flashing_animation(text, font, color):
     images = []
     for char in text:
-        img = Image.new('RGB', animation_size)
+        img = Image.new('RGB', ANIMATION_SIZE)
         draw = ImageDraw.Draw(img)
         size = draw.textsize(char, font)
-        x = (width - size[0]) // 2
-        y = (height - size[1]) // 2
+        x = (ANIMATION_WIDTH - size[0]) // 2
+        y = (ANIMATION_HEIGHT - size[1]) // 2
         draw.text((x, y), char, color, font)
         images.append(img)
+    return images
+
+def get_black_frame():
+    size = ANIMATION_SIZE
+    img = Image.new('RGB', size)
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((0, 0, ANIMATION_WIDTH, ANIMATION_HEIGHT),  fill = (0,0,0))
+    return img
+
+def get_text_size(text, font):
+    img_text = Image.new('RGB', ANIMATION_SIZE)
+    draw = ImageDraw.Draw(img_text)
+    text_size = draw.textsize(text, font)
+    return text_size
+
+def scrolling_animation(text, font, color):
+    text_size = get_text_size(text, font)
+
+    images = []
+    img_text = Image.new('RGB', text_size)
+    draw = ImageDraw.Draw(img_text)
+    draw.text((0, 0), text, color, font)
+
+    src_x = 0
+    dst_x = ANIMATION_WIDTH - 1
+    copy_width = 1
+    black_frame = get_black_frame()
+    images.append(black_frame)
+    while True:
+        img_temp = img_text.crop((src_x, 0, src_x + copy_width, ANIMATION_HEIGHT))
+        img = Image.new('RGB', ANIMATION_SIZE)
+        img.paste(img_temp, (dst_x, 0, dst_x + copy_width, ANIMATION_HEIGHT))
+        images.append(img)
+
+        copy_width = min(copy_width + 1, ANIMATION_WIDTH)
+        dst_x = min(dst_x - 1, ANIMATION_WIDTH)
+        dst_x = max(dst_x, 0)
+        if dst_x == 0:
+            src_x += 1
+        if src_x > text_size[0]:
+            break
+    return images
+
+def text_to_animation(text, font_size, color, delay, scroll=True, filename=None):
+    if platform == 'win32':
+        font_path = 'C:\\Windows\\Fonts\\segoeui.ttf'
+    else:
+        font_path = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf'
+    font = ImageFont.truetype(font_path, font_size)
+    if scroll:
+        images = scrolling_animation(text, font, color)
+    else:
+        images = flashing_animation(text, font, color)
+
     animation = images_to_animation(images, delay)
     animation.name = text
-    Animation.save(text, animation)
+    if not filename:
+        filename = text
+    Animation.save(filename, animation)
+
 
 
 # e.g. 
-# python txt2anim.py "HAPPY NEW YEAR" 15, #00ff00 500 
+# python txt2anim.py "HAPPY NEW YEAR" 15 "#00ff00" 500 True  "happy_new_year"
 if __name__ == '__main__':
     fire.Fire(text_to_animation)
 
